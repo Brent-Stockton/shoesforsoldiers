158a6ac6a90f37bef276495e79b0c05d16bb4820
Brent-Stockton <brentstockton1@gmail.com>
2023-12-28 21:21:51 -0700

    fix globals

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,8 +1,6 @@
 import { Inter } from "next/font/google";
 import Head from "next/head";
 
-import "./globals.css";
-
 const inter = Inter({ subsets: ["latin"] });
 
 export const metadata = {
--- app/page.tsx
+++ app/page.tsx
@@ -1,7 +1,5 @@
 import App from "./App";
-
 import "../styles/global.css";
-import build from "next/dist/build";
 
 export default function Home() {
   return <App />;
